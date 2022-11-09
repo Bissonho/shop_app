e218c4a4a9a9eecd1413b4969f6af9a9ed05b7d8
Paulo Bissonho <37625379+Bissonho@users.noreply.github.com>
2022-11-08 23:33:38 -0300

    fidex auth _token

--- lib/providers/product_list.dart
+++ lib/providers/product_list.dart
@@ -7,11 +7,11 @@ import 'package:shop_app/utils/constante.dart';
 import '../exceptions/http_exception.dart';
 
 class ProductList with ChangeNotifier {
-  String _token;
-
-  ProductList(this._token, this._itens);
-
+  final String _token;
+  final String _userId;
   List<Product> _itens = [];
+
+  ProductList([this._token = '', this._itens = const [], this._userId = '']);
 
   List<Product> get itens {
     return [..._itens];
@@ -34,7 +34,6 @@ class ProductList with ChangeNotifier {
           "description": product.description,
           "price": product.price,
           "imageUrl": product.imageUrl,
-          "isFavorite": product.isFavorite
         },
       ),
     );
@@ -57,8 +56,8 @@ class ProductList with ChangeNotifier {
       final product = _itens[index];
       _itens.remove(product);
       notifyListeners();
-      final response = await http.delete(
-          Uri.parse('${constantes.PRODUCT_BASE_URL}/${product.id}.json'));
+      final response = await http.delete(Uri.parse(
+          '${constantes.PRODUCT_BASE_URL}/${product.id}.json?auth=$_token'));
 
       if (response.statusCode >= 400) {
         _itens.insert(index, product);
@@ -72,18 +71,27 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     _itens.clear();
-    final response =
-        await http.get(Uri.parse('${constantes.PRODUCT_BASE_URL}.json'));
+    final response = await http
+        .get(Uri.parse('${constantes.PRODUCT_BASE_URL}.json?auth=$_token'));
     if (response.body == 'null') return;
+
+    final favResponse = await http.get(
+      Uri.parse('${constantes.USER_FAVORITE_URL}/$_userId/.json?auth=$_token'),
+    );
+
+    Map<String, dynamic> favData =
+        favResponse.body == 'null' ? {} : jsonDecode(favResponse.body);
+
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productid, productdata) {
+      final isFavorite = favData[productid] ?? false;
       _itens.add(
         Product(
             id: productid,
             name: productdata['name'],
             description: productdata['description'],
             price: productdata['price'],
-            isFavorite: productdata['isFavorite'],
+            isFavorite: isFavorite,
             imageUrl: productdata['imageUrl']),
       );
     });
@@ -137,14 +145,14 @@ class ProductList with ChangeNotifier {
       _itens[index] = product;
 
       try {
-        final response = await http.patch(
+        final response = await http.put(
           Uri.parse(
-              '${constantes.PRODUCT_BASE_URL}/${product.id}.json?auth=$_token'),
-          body: jsonEncode(
-            {'isFavorite': product.isFavorite},
-          ),
+              '${constantes.USER_FAVORITE_URL}/$_userId/${product.id}.json?auth=$_token'),
+          body: jsonEncode(product.isFavorite),
         );
         if (response.statusCode >= 400) {
+          print('User id$_userId');
+          print(response.body);
           return Future.value(false);
         }
         notifyListeners();
